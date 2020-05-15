00d2fba27ec4fdc68508235d51a08e27b02fc280
Morten Laursen <mortendlaursen@gmail.com>
2020-05-15 23:28:17 +0200

    added everything

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    console.log("DEMOMDE");
+    var flkty = new Flickity(".carousel", {
+        // re-position cells when images load
+        imagesLoaded: true,
+        wrapAround: true,
+        fullscreen: true,
+        fade: true,
+    });
+});
+
+// bs carousel touch slide
+// https://github.com/maaaaark/bcSwipe/blob/master/jquery.bcSwipe.js
